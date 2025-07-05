6fa925eb51311d297ab5029395a5142b3ce215fa
Savely Burkovskiy <savelybur.work@gmail.com>
2025-07-05 20:05:17 +0700

    Added very simple main menu with theme stored in TGUI library. For future: create own theme

--- include/main.hpp
+++ include/main.hpp
@@ -2,5 +2,11 @@
 #define MAIN_HPP
 
 #include <SFML/Graphics.hpp>
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
+#include <cstdlib>
+
+void loadMainMenu(tgui::BackendGui& gui);
+void loadGameSelect(tgui::BackendGui& gui);
 
 #endif
