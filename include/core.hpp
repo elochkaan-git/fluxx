7b5d053618148284bdb220f17a4fe4a5b39f11de
Alexander Elochka <elochkaan@gmail.com>
2025-07-09 15:42:39 +0700

    0.0.18 - Вынесение всего, что связано с внутренностями игры, в отдельный заголовочный файл

---
+++ include/core.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <cstdlib>
+
+#include "card.hpp"
+#include "player.hpp"
+#include "state.hpp"
--- include/main.hpp
+++ include/main.hpp
@@ -1,17 +1,9 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
-#include <SFML/Graphics.hpp>
+
 #include <TGUI/TGUI.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
-#include <vector>
-#include <cstdlib>
-
-#include "card.hpp"
-#include "player.hpp"
-#include "state.hpp"
-
-
 void loadMainMenu(tgui::BackendGui& gui);
 void loadGameSelect(tgui::BackendGui& gui);
 
