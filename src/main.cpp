b3bd0cdf0dc7fe61e5564cefb28c9a5e5ff383d2
Alexander Elochka <elochkaan@gmail.com>
2025-07-09 16:42:05 +0700

    0.0.22 - Мелкие изменения

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
 #include "main.hpp"
 #include "core.hpp"
 
-
-void update(unsigned short int numOfPlayers)
-{
-    std::vector<Player*> players(numOfPlayers);
-    for(Player* p : players) p = new Player();
-    State state(players);
-
-
-    for(Player* p : players) delete p;
-}
-
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "Fluxx", sf::Style::Titlebar | sf::Style::Close);
