dacc649a511769a013de0f832ef964f097aebe57
Alexander Elochka <elochkaan@gmail.com>
2025-07-09 15:40:19 +0700

    0.0.17 - Добавлен метод для получения карт в начале хода

--- src/player.cpp
+++ src/player.cpp
@@ -4,7 +4,19 @@
 Player::Player() : hand(0), tableThemes(0) {}
 
 void Player::takeCards(State& state) {
-    for(unsigned short int i = 0; i < 3; ++i) {
-        this->hand.push_back(state.getCard());
+    if(isFirstTurn)
+    { 
+        for(unsigned short int i = 0; i < 3; ++i) 
+        {
+            this->hand.push_back(state.getCard());
+        }
+        isFirstTurn = false;
+    }
+    else
+    {
+        for(unsigned short int i = 0; i < state.getRules()->take; ++i) 
+        {
+            this->hand.push_back(state.getCard());
+        }
     }
 }
