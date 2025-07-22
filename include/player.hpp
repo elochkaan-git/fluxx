cbed54195286f0324fe4a6cb0651263ffeaaec79
Alexander Elochka <elochkaan@gmail.com>
2025-07-22 18:24:44 +0700

    Add comments Add method for getting number of cards in hand

--- include/player.hpp
+++ include/player.hpp
@@ -3,19 +3,25 @@
 #include "card.hpp"
 #include "state.hpp"
 
+// Объявление класса State для избежания цикличного включения
 class State;
 
+/**
+ * @brief Класс для представления игрока
+ *
+ */
 class Player
 {
 private:
-    std::vector<Cards> hand;
-    std::vector<unsigned short int> tableThemes;
+    std::vector<unsigned short int> hand;        // Карты на руки
+    std::vector<unsigned short int> tableThemes; // Темы на столе
     bool firstTurn = true;
 
 public:
     Player();
     void takeCards(State& state);
     const std::vector<unsigned short int> getThemes() const;
+    const unsigned short int numOfCardsInHands() const;
     const void addTheme(unsigned short int theme);
     const bool isFirstTurn() const;
 };
--- src/player.cpp
+++ src/player.cpp
@@ -3,41 +3,78 @@
 #include "state.hpp"
 #include <vector>
 
+/**
+ * @brief Конструктор класса Player
+ *
+ * Создает новый объект класса Player с пустыми рукой и темами на столе
+ */
 Player::Player()
   : hand()
   , tableThemes(0)
 {
 }
 
+/**
+ * @brief Добавляет в руку игрока необходимое число карт
+ *
+ * @param state Игровое состояние
+ */
 void
 Player::takeCards(State& state)
 {
     if (firstTurn) {
         for (unsigned short int i = 0; i < 3; ++i) {
-            this->hand.push_back(state.getCard());
+            this->hand.push_back(state.getCardFromTop());
         }
         firstTurn = false;
     } else {
         for (unsigned short int i = 0; i < state.getRules()->take; ++i) {
-            this->hand.push_back(state.getCard());
+            this->hand.push_back(state.getCardFromTop());
         }
     }
 }
 
+/**
+ * @brief Возвращает набор Id карт-тем, лежащих у игрока на столе
+ *
+ * @return const std::vector<unsigned short int> Набор Id карт-тем
+ */
 const std::vector<unsigned short int>
 Player::getThemes() const
 {
     return this->tableThemes;
 }
 
+/**
+ * @brief Проверяет, первый ли ход у игрока
+ *
+ * @return true Если первый
+ * @return false Если не первый
+ */
 const bool
 Player::isFirstTurn() const
 {
     return firstTurn;
 }
 
+/**
+ * @brief Добавляет Id карты-темы в вектор Id карт-тем, лежащих на столе
+ *
+ * @param themeId Id сыгранной карты-темы
+ */
 const void
-Player::addTheme(unsigned short int theme)
+Player::addTheme(unsigned short int themeId)
 {
-    tableThemes.push_back(theme);
+    tableThemes.push_back(themeId);
+}
+
+/**
+ * @brief Возвращает количество карт в руке
+ *
+ * @return const unsigned short int Количество карт в руке
+ */
+const unsigned short int
+Player::numOfCardsInHands() const
+{
+    return this->hand.size();
 }
